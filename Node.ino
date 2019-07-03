35fd03e9468395cba17225b9724bbb637c797f8a
Erik <eri.gim2008@gmail.com>
2019-07-03 15:35:11 -0300

    Lectura EEPROM Red

--- Node.ino
+++ Node.ino
@@ -2,26 +2,97 @@
 #include <Pagina.h>                   //LIBRERIA DE LAS PAGINAS WEB
 #include <ESP8266WiFi.h>              //LIBRERIAS DEL NODE
 #include <ESP8266WebServer.h>         //LIBRERIA PARA SERVIDOR WIFI
+#include <EEPROM.h>
+
+struct Acceso
+{
+   char Usuario;
+   char Contrasena;
+};
+
+struct Wifi
+{
+   const char* nombre;
+   const char* contrasena;
+   int ip1;
+   int ip2;
+   int ip3;
+   int ip4;
+   int mascara1;
+   int mascara2;
+   int mascara3;
+   int mascara4;
+   int puerta1;
+   int puerta2;
+   int puerta3;
+   int puerta4;
+   int dns1;
+   int dns2;
+   int dns3;
+   int dns4;
+};
+
+struct Broker
+{
+   char ip;
+   char idCliente;
+   char usuario;
+   char contrasena;
+   char prefijo;
+};
 
 
-String ssid = "TP-LINK_33976C";       //NOMBRE DE LA RED WIFI A CONECTAR
-String contra = "";                   //CONTRASEÑA DE LA RED
+
+
+
+
+
+
 Pagina pag;                           //INICIALIZACIÓN DEL OBJETO PAGINA
 
-ESP8266WebServer server(80);          
+
+String ssid = "";
+String contra  = "";
+
+ESP8266WebServer server(80);
+WiFiClient clienteESP; 
+         
 String usuario = "Usuario";           //USUARIO PARA ACCEDER A LA PÁGINA DE CONFIGURACIÓN
 String pass = "Usuario";              //CONTRASEÑA PARA ACCEDER
 
 String mensaje = "";                  //MENSAJE A MOSTRAR LUEGO DE APLICAR CAMBIOS
 
+
+
 void setup()
 {
-  Serial.begin(115200);                
-  //WiFi.begin(ssid, contra);          
-  WiFi.mode(WIFI_AP);                 //CONFIGURO EL NODE COMO ACCESS POINT(SERVIDOR)
-  boolean servidor = WiFi.softAP("Node", "esp82666");     
-  if(servidor == true)
+  EEPROM.begin(512);
+  Serial.begin(115200);
+  pinMode(0, INPUT);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+
+  
+  if(digitalRead(0) == LOW)
   {
+    Serial.println("INICIADO EN MODO SERVIDOR");
+    digitalWrite(2, LOW);
+    delay(100);
+    digitalWrite(2, HIGH);
+    delay(100);
+    digitalWrite(2, LOW);
+    delay(100);
+    digitalWrite(2, HIGH);
+
+
+
+       
+    WiFi.mode(WIFI_AP);                 //CONFIGURO EL NODE COMO ACCESS POINT(SERVIDOR)
+    boolean servidor = WiFi.softAP("Node", "esp82666");
+
+
+   if(servidor == true)
+   {
     IPAddress ipLocal(192,168,4,108);
     IPAddress puertaAcceso(192,168,4,9);
     IPAddress mascaraSubnet(255,255,255,0);
@@ -41,21 +112,65 @@ void setup()
   server.on("/reset", HTTP_POST, Reinicio);
   server.onNotFound(NoEncontrado);
   
-  server.begin();                       
+  server.begin();  
+
+  while (true) 
+  {
+      server.handleClient();
+  }
+}
+    Serial.println("INICIADO EN MODO CLIENTE");
+    ssid = Leer(0);
+    contra = Leer(50);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, contra);
+    
+
+    Serial.print("Intentando conectar a red: ");
+    Serial.println(ssid);
+    Serial.print("Contraseña: ");
+    Serial.println(contra);
+          
+    Serial.print("CONECTANDO");
+    int intento = 0;
+    while(WiFi.status() != WL_CONNECTED)
+    {
+      if(intento < 50)
+      {
+        Serial.print(".");
+        digitalWrite(2, LOW);
+        delay(250);
+        digitalWrite(2, HIGH);
+        delay(250);
+      }else
+        break;
+       intento++;
+     }
+     if (intento >= 50){
+      Serial.println("IMPOSIBLE CONECTAR");
+     }
+     else
+     {
+      Serial.print("\nConectado a NUEVA red: ");
+      Serial.println(WiFi.SSID());
+      Serial.print("NUEVA Direccion IP: ");
+      Serial.println(WiFi.localIP());
+      digitalWrite(2, LOW);
+     }
+     
+
+                     
 }
 
 
-void loop()
-{
-  server.handleClient();                  
-}
+void loop(){}
 
 
 void Acceso()
 {
    server.send(200, "text/html", pag.PInicio().c_str()); 
 }
-
 
 void Congifuracion()
 {
@@ -117,15 +232,17 @@ void CambioRed()
       
       String nSSID = server.arg("ssid_ap");
       String ncontra = server.arg("password_ap");
-
-      ssid = nSSID;
-      contra = ncontra;
+      
+      
+      Grabar(0, nSSID);
+      Grabar(50, ncontra);
+      
+      
       
       if(server.arg("ip") == "dinamica")
       {
         Serial.println("SELECCIONADA RED DINAMICA");
         WiFi.disconnect();
-        WiFi.begin(ssid, contra);
       }
       else if(server.arg("ip") == "estatica")
       {
@@ -169,39 +286,39 @@ void CambioRed()
         Serial.println("NO RECIBO RED");
       }
 
-      Serial.print("\nRE-CONECTANDO");
-      int intento = 0;
-      while(WiFi.status() != WL_CONNECTED)
-      {
-        if((intento > 50) || WiFi.status() == WL_NO_SSID_AVAIL)
-        {
-          Serial.print("\nNo se pudo conectar a la nueva red: ");
-          mensaje += "<li>NO SE PUDO CONECTAR A LA RED SELECCIONADA</li>";
-          WiFi.mode(WIFI_AP);
-          return;
-        }
-        Serial.print(".");
-        delay(100);
-        intento++;
-      }
-      IPAddress gateway;
-      IPAddress subnet;
-      subnet = WiFi.subnetMask();
-      Serial.print("\nMASCARA: ");
-      Serial.println(subnet);
+      //Serial.print("\nRE-CONECTANDO");
+      //int intento = 0;
+      //while(WiFi.status() != WL_CONNECTED)
+      //{
+      //  if((intento > 50) || WiFi.status() == WL_NO_SSID_AVAIL)
+      //  {
+      //    Serial.print("\nNo se pudo conectar a la nueva red: ");
+      //    mensaje += "<li>NO SE PUDO CONECTAR A LA RED SELECCIONADA</li>";
+      //    WiFi.mode(WIFI_AP);
+      //    return;
+      //  }
+      //  Serial.print(".");
+      //  delay(100);
+      //  intento++;
+      //}
+      //IPAddress gateway;
+      //IPAddress subnet;
+      //subnet = WiFi.subnetMask();
+      //Serial.print("\nMASCARA: ");
+      //Serial.println(subnet);
 
-      gateway = WiFi.gatewayIP();
-      Serial.print("PUERTA DE ENLACE: ");
-      Serial.println(gateway);
+      //gateway = WiFi.gatewayIP();
+      //Serial.print("PUERTA DE ENLACE: ");
+      //Serial.println(gateway);
       
-      mensaje += "<li>CONECTADO A NUEVA RED: ";
-      mensaje += WiFi.SSID();
-      mensaje += "</li>";
+      //mensaje += "<li>CONECTADO A NUEVA RED: ";
+     // mensaje += WiFi.SSID();
+     // mensaje += "</li>";
       
-      Serial.print("\nConectado a NUEVA red: ");
-      Serial.println(WiFi.SSID());
-      Serial.print("NUEVA Direccion IP: ");
-      Serial.println(WiFi.localIP());     
+      //Serial.print("\nConectado a NUEVA red: ");
+      //Serial.println(WiFi.SSID());
+      //Serial.print("NUEVA Direccion IP: ");
+      //Serial.println(WiFi.localIP());     
     }
 }
 
@@ -225,8 +342,48 @@ void Reinicio()
 {
   Serial.println("REINICIANDO NODE MCU");
   server.send(200, "text/html", pag.PMensaje("REINICIANDO NODE MCU"));
-  ESP.reset();
+  ESP.restart();
 }
+
+
+void Grabar(int dir, String dato)
+{
+  int espacio = dato.length(); 
+  char almac[50]; 
+  dato.toCharArray(almac, espacio+1);
+  for (int i = 0; i < espacio; i++) {
+    EEPROM.write(dir+i, almac[i]);
+  }
+  for (int i = espacio; i < 50; i++) {
+    EEPROM.write(dir+i, 255);
+  }
+  EEPROM.commit();
+}
+
+String Leer(int dir)
+{
+   byte dato;
+   String datoString;
+   for (int i = dir; i < dir+50; i++) {
+      dato = EEPROM.read(i);
+      if (dato != 255) {
+        datoString += (char)dato;
+      }
+   }
+   return datoString;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 void NoEncontrado()
 {
