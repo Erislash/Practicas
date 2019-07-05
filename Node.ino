e044ae68cfb058714d6ea5da02b490cba524ee73
Erik <eri.gim2008@gmail.com>
2019-07-05 16:14:16 -0300

    EEPROM: Red D/E, Cambio de Acceso, Broker MQTT

--- Node.ino
+++ Node.ino
@@ -9,21 +9,55 @@ Pagina pag;                           //INICIALIZACIÓN DEL OBJETO PAGINA
 String ssid = "";
 String contra  = "";
 
+
+
 ESP8266WebServer server(80);
-WiFiClient clienteESP; 
+WiFiClient clienteESP;
+PubSubClient client(clienteESP);
+
+
          
-String usuario = "Usuario";           //USUARIO PARA ACCEDER A LA PÁGINA DE CONFIGURACIÓN
-String pass = "Usuario";              //CONTRASEÑA PARA ACCEDER
+String usuario;           //USUARIO PARA ACCEDER A LA PÁGINA DE CONFIGURACIÓN
+String pass;              //CONTRASEÑA PARA ACCEDER
+
+
+
+
+///////////////////////////////////////////////////////////////////////////CONFIGURACIÓN MQTT/////////////////////////////////////////////7
+String servidorMQTT = "";//"postman.cloudmqtt.com";
+int puertoMQTT = 0;//13726;
+String usuarioMQTT = "";//"vpqvmuwo";
+String contraMQTT = "";//"X4tgq9y_cNba";
+String temaMQTT = "";//"esp8266/test";
+///////////////////////////////////////////////////////////////////////////COMIENZA MQTT/////////////////////////////////////////////7
+
 
 String mensaje = "";                  //MENSAJE A MOSTRAR LUEGO DE APLICAR CAMBIOS
 
 void setup()
 {
+  EEPROM.end();
   EEPROM.begin(512);
+  
   Serial.begin(115200);
+  Serial.printf("\n\n\n");
   pinMode(0, INPUT);
   pinMode(2, OUTPUT);
   digitalWrite(2, HIGH);
+
+  Serial.println(Leer(150, 10));
+  if(Leer(150, 10) != "Cambiado"){
+    Grabar(120, "Admin", 15);
+    Grabar(135, "Admin", 15);
+  }
+  usuario = Leer(120, 15);
+  pass = Leer(135, 15);
+
+  Serial.println(Leer(120, 15));
+  Serial.println(Leer(135, 15));
+  Serial.println("AHORA HAY ESTO: ");
+  Serial.println(Leer(150, 10));
+  
 
   
   if(digitalRead(0) == LOW)
@@ -73,20 +107,20 @@ void setup()
   }
 }
     Serial.println("INICIADO EN MODO CLIENTE");
-    ssid = Leer(0);
-    contra = Leer(50);
+    ssid = Leer(0, 15);
+    contra = Leer(15, 15);
 
     WiFi.disconnect();
     
-    if(Leer(420) == "estatica")
+    if(Leer(110, 10) == "estatica")
     {
       Serial.println("RED ESTATICA CONFIGURADA");
       
       
-      IPAddress ipEstatica(Leer(100).toInt(), Leer(120).toInt(), Leer(140).toInt(), Leer(160).toInt());
-      IPAddress compuerta((Leer(180).toInt()), (Leer(200).toInt()), (Leer(220).toInt()), (Leer(240).toInt()));
-      IPAddress mascara((Leer(260).toInt()), (Leer(280).toInt()), (Leer(300).toInt()), (Leer(320).toInt()));
-      IPAddress dns((Leer(340).toInt()), (Leer(360).toInt()), (Leer(380).toInt()), (Leer(400).toInt()));
+      IPAddress ipEstatica(Leer(30, 5).toInt(), Leer(35, 5).toInt(), Leer(40, 5).toInt(), Leer(45, 5).toInt());
+      IPAddress compuerta((Leer(50, 5).toInt()), (Leer(55, 5).toInt()), (Leer(60, 5).toInt()), (Leer(65, 5).toInt()));
+      IPAddress mascara((Leer(70, 5).toInt()), (Leer(75, 5).toInt()), (Leer(80, 5).toInt()), (Leer(85, 5).toInt()));
+      IPAddress dns((Leer(90, 5).toInt()), (Leer(95, 5).toInt()), (Leer(100, 5).toInt()), (Leer(105, 5).toInt()));
       
       Serial.println(ipEstatica);
       Serial.println(compuerta);
@@ -149,11 +183,82 @@ void setup()
 
       gateway = WiFi.gatewayIP();
       Serial.print("PUERTA DE ENLACE: ");
-      Serial.println(gateway);       
+      Serial.println(gateway);
+
+
+
+
+///////////////////////////////////////////////////////////////////////////COMIENZA MQTT/////////////////////////////////////////////7
+    servidorMQTT = Leer(160, 30);
+    puertoMQTT = Leer(190, 30).toInt();
+    usuarioMQTT = Leer(220, 30);
+    contraMQTT = Leer(250, 30);
+    temaMQTT = Leer(280, 30);
+    
+    Serial.println("MQTT");
+    Serial.println(servidorMQTT);
+    Serial.println(puertoMQTT);
+    Serial.println(usuarioMQTT);
+    Serial.println(contraMQTT);
+    Serial.println(temaMQTT);
+
+    client.setServer(servidorMQTT.c_str(), puertoMQTT);
+    client.setCallback(Llamada);
+
+    while (!client.connected()) {
+    Serial.println("Conectando a MQTT...");
+ 
+    if (client.connect("ClienteESP8266", usuarioMQTT.c_str(), contraMQTT.c_str())) {
+ 
+      Serial.println("Conectado");  
+      client.publish(temaMQTT.c_str(), "Hola desde el ESP8266");
+      client.subscribe(temaMQTT.c_str());
+ 
+    } else {
+ 
+      Serial.print("Fallo en la conexión");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+ 
+  
+  client.setCallback(Llamada);
+}
+
+void Llamada(char* topic, byte* payload, unsigned int length) {
+  String orden = "";
+ 
+  Serial.print("Ha llegado un mensaje desde el tema: ");
+  Serial.println(topic);
+ 
+  Serial.print("Mensaje:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    orden += (char)payload[i];
+    
+  }
+  Serial.println(orden);
+    if(orden == "encender")
+    
+      digitalWrite(2, LOW);
+    else
+      digitalWrite(2, HIGH);
+  
+  Serial.println();
+  Serial.println("-----------------------");
 }
 
 
-void loop(){}
+
+void loop()
+{
+  client.loop();
+}
+/////////////////////////////////////////////////////////////////////////// TERMINA MQTT/////////////////////////////////////////////7
+
+
+
 
 
 void Acceso()
@@ -164,7 +269,11 @@ void Acceso()
 void Congifuracion()
 {
   //COMPRUEBO SI EL ACCESO ES CORRECTO
-  
+
+  Serial.printf("Usuario de acceso: ");
+  Serial.println(usuario);
+  Serial.printf("Contraseña de acceso: ");
+  Serial.println(pass);
   if(!server.hasArg("usuario") || !server.hasArg("password") || server.arg("usuario") == NULL || server.arg("password") == NULL)
   {
     server.send(400, "text/html", pag.PMensaje("ACCESO INCOMPLETO"));
@@ -172,6 +281,7 @@ void Congifuracion()
   }
   if(server.arg("usuario") == usuario && server.arg("password") == pass)
   {
+    
     //SI USUARIO Y CONTRASEÑA SON CORRECTOS, ENVÍO LA PÁGINA DE CONFIGURACIÓN
     server.send(200, "text/html", pag.PConfiguracion().c_str());
   }
@@ -197,6 +307,7 @@ void CambioRed()
 {
     if (!server.hasArg("ssid_ap") || !server.hasArg("password_ap") || server.arg("ssid_ap") == NULL)
     {
+      mensaje += "<li>NO SE MODIFICÓ LA RED</li>";
       return;
     }
     else if(server.hasArg("ssid_ap") && server.hasArg("password_ap"))
@@ -207,14 +318,14 @@ void CambioRed()
       String ncontra = server.arg("password_ap");
       
       
-      Grabar(0, nSSID);
-      Grabar(50, ncontra);
+      Grabar(0, nSSID, 15);
+      Grabar(15, ncontra, 15);
       
       
       if(server.arg("ip") == "dinamica")
       {
         Serial.println("SELECCIONADA RED DINAMICA");
-        Grabar(260, "dinamica");
+        Grabar(110, "dinamica", 10);
       }
       else if(server.arg("ip") == "estatica")
       {
@@ -239,23 +350,23 @@ void CambioRed()
           String mascara[4] = {server.arg("puerta_est_1"), server.arg("puerta_est_2"), server.arg("puerta_est_3"), server.arg("puerta_est_4")};
           String dns[4] = {server.arg("dns_est_1"), server.arg("dns_est_2"), server.arg("dns_est_3"), server.arg("dns_est_4")};
 
-          Grabar(100, ipEstatica[0]);
-          Grabar(120, ipEstatica[1]);
-          Grabar(140, ipEstatica[2]);
-          Grabar(160, ipEstatica[3]);
-          Grabar(180, compuerta[0]);
-          Grabar(200, compuerta[1]);
-          Grabar(220, compuerta[2]);
-          Grabar(240, compuerta[3]);
-          Grabar(260, mascara[0]);
-          Grabar(280, mascara[1]);
-          Grabar(300, mascara[2]);
-          Grabar(320, mascara[3]);
-          Grabar(340, dns[0]);
-          Grabar(360, dns[1]);
-          Grabar(380, dns[2]);
-          Grabar(400, dns[3]);
-          Grabar(420, "estatica");
+          Grabar(30, ipEstatica[0], 5);
+          Grabar(35, ipEstatica[1], 5);
+          Grabar(40, ipEstatica[2], 5);
+          Grabar(45, ipEstatica[3], 5);
+          Grabar(50, compuerta[0], 5);
+          Grabar(55, compuerta[1], 5);
+          Grabar(60, compuerta[2], 5);
+          Grabar(65, compuerta[3], 5);
+          Grabar(70, mascara[0], 5);
+          Grabar(75, mascara[1], 5);
+          Grabar(80, mascara[2], 5);
+          Grabar(85, mascara[3], 5);
+          Grabar(90, dns[0], 5);
+          Grabar(95, dns[1], 5);
+          Grabar(100, dns[2], 5);
+          Grabar(105, dns[3], 5);
+          Grabar(110, "estatica", 10);
         }
       }
       else
@@ -269,13 +380,19 @@ void CambioAcceso()
 {
   if (!server.hasArg("usuario_web") || !server.hasArg("password_web") || server.arg("usuario_web") == NULL || server.arg("password_web") == NULL)
     {
-      mensaje += "<li>NO SE MODIFICO EL USUARIO NI CONTRASEÑA<li>";
+      mensaje += "<li>NO SE MODIFICO EL USUARIO NI CONTRASEÑA</li>";
       return;
     }
     else
     {
       usuario = server.arg("usuario_web");
       pass = server.arg("password_web");
+
+      Grabar(120, usuario, 15);
+      Grabar(135, pass, 15);
+      Grabar(150, "Cambiado", 10);
+
+      Serial.println(Leer(150, 10));
       Serial.println("USUARIO Y CONTRASEÑA MODIFICADOS");
       mensaje += "<li>USUARIO Y CONTRASEÑA MODIFICADOS</li>";
     }
@@ -293,6 +410,12 @@ void CambioMQTT()
     }
     else
     {
+      Grabar(160, server.arg("mqtt_server"), 30);
+      Grabar(190, server.arg("id_client"), 30);
+      Grabar(220, server.arg("usuario_mqtt"), 30);
+      Grabar(250, server.arg("password_mqtt"), 30);
+      Grabar(280, server.arg("prefijo_mqtt"), 30);
+      
       Serial.println("SE HA MODIFICADO EL BROKER MQTT");
       mensaje += "<li>SE MODIFICÓ EL BROKER MQTT</li>";
     }
@@ -305,41 +428,32 @@ void Reinicio()
   ESP.restart();
 }
 
-//GRABA EN LA EEPROM.
-//dir: posicion de memoria.
-//dato: dato que se quiera guardar
-//uso: bits de uso que se le asignan
-void Grabar(int ph_dir, String ph_dato)  //, int uso)
+
+void Grabar(int dir, String dato, int uso)
 {
-  EEPROM.put(ph_dir, ph_dato);
-  //int espacio = dato.length(); 
-  //char almac[uso]; 
-  //dato.toCharArray(almac, espacio+1);
-  //for (int i = 0; i < espacio; i++) {
-  //  EEPROM.write(dir+i, almac[i]);
-  //}
-  //for (int i = espacio; i < uso; i++) {
-  //  EEPROM.write(dir+i, 255);
- // }
+  int espacio = dato.length(); 
+  char almac[uso]; 
+  dato.toCharArray(almac, espacio+1);
+  for (int i = 0; i < espacio; i++) {
+    EEPROM.write(dir+i, almac[i]);
+  }
+  for (int i = espacio; i < uso; i++) {
+    EEPROM.write(dir+i, 255);
+  }
   EEPROM.commit();
 }
 
-//LEE LA EEPROM.
-//dir: posicion de memoria.
-//uso: bits que se le asignaron en la grabacion
-String Leer(int ph_dir) //, int uso)
+String Leer(int dir, int uso)
 {
-  String dato = "";
-  EEPROM.get(ph_dir, dato);
-  //byte dato;
-   //String datoString;
-   //for (int i = dir; i < dir+uso; i++) {
-   //   dato = EEPROM.read(i);
-  //    if (dato != 255) {
-   //     datoString += (char)dato;
-   //   }
-   //}
-   return dato;
+   byte dato;
+   String datoString;
+   for (int i = dir; i < dir+uso; i++) {
+      dato = EEPROM.read(i);
+      if (dato != 255) {
+        datoString += (char)dato;
+      }
+   }
+   return datoString;
 }
 
 
